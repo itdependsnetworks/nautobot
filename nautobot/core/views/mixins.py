3feeb89d0136ddd9de09be1a39390b25be7b7f78
Sridevi-NTC <sridevi.cheekatla@networktocode.com>
2025-07-15 14:29:30 +0000

    fixed ruff issue

--- nautobot/core/views/mixins.py
+++ nautobot/core/views/mixins.py
@@ -1551,7 +1551,6 @@ class ComponentCreateViewMixin(NautobotViewSetMixin, mixins.CreateModelMixin):
             # Check that the new object conforms with any assigned object-level permissions
             queryset.get(pk=obj.pk)
 
-
             msg = f'{"Created" if object_created else "Modified"} {queryset.model._meta.verbose_name}'
             self.logger.info(f"{msg} {obj} (PK: {obj.pk})")
             try:
@@ -1587,7 +1586,6 @@ class ComponentCreateViewMixin(NautobotViewSetMixin, mixins.CreateModelMixin):
         if request.method == "POST":
             return self.perform_create(request, *args, **kwargs)
         return Response(context)
-
 
     def perform_create(self, request, *args, **kwargs):  # pylint: disable=arguments-differ
         """
